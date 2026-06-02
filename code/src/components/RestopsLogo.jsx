2bfbf197c32aeec0f29396eeb944f33f1f3252b2
Siva Sai Pavan Karthik Uppalapati <uppalapatisivasaipavankarthik@gmail.com>
2026-06-02 17:51:19 -0400

    02-06/12

--- code/src/components/RestopsLogo.jsx
+++ code/src/components/RestopsLogo.jsx
@@ -4,15 +4,15 @@ export default function RestopsLogo({ className = "h-12", showText = true }) {
   return (
     <div className={`flex items-center gap-3 ${className}`}>
       {/* Logo Mark */}
-      <img src="/app-logo.png" alt="Restops Logo" className="h-full w-auto object-contain shrink-0 dark:hidden block" />
-      <img src="/app-logo-dark.png" alt="Restops Logo" className="h-full w-auto object-contain shrink-0 hidden dark:block" />
+      <img src="/app-logo.png" alt="Restops Logo" className="h-full w-auto object-contain shrink-0 transform scale-[2] origin-left dark:hidden block" />
+      <img src="/app-logo-dark.png" alt="Restops Logo" className="h-full w-auto object-contain shrink-0 transform scale-[2] origin-left hidden dark:block" />
 
       {/* Typography / Name Image */}
       {showText && (
-        <>
-          <img src="/app-name.png" alt="Restops Name" className="h-[60%] w-auto object-contain shrink-0 mt-0.5 dark:hidden block" />
-          <img src="/app-name-dark.png" alt="Restops Name" className="h-[60%] w-auto object-contain shrink-0 mt-0.5 hidden dark:block" />
-        </>
+        <div className="ml-4 h-full flex items-center">
+          <img src="/app-name.png" alt="Restops Name" className="h-[80%] w-auto object-contain shrink-0 transform scale-[1.5] origin-left dark:hidden block" />
+          <img src="/app-name-dark.png" alt="Restops Name" className="h-[80%] w-auto object-contain shrink-0 transform scale-[1.5] origin-left hidden dark:block" />
+        </div>
       )}
     </div>
   );
