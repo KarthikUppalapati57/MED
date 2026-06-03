e74b2be9c8912854eda2a4d974b70c4f3e2d5988
Siva Sai Pavan Karthik Uppalapati <uppalapatisivasaipavankarthik@gmail.com>
2026-06-03 12:05:06 -0400

    03-06/05

--- code/src/components/RestopsLogo.jsx
+++ code/src/components/RestopsLogo.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 export default function RestopsLogo({ className = "h-12", showText = true }) {
   return (
     <div className={`flex items-center gap-3 ${className}`}>
-      {/* Typography / Name Image */}
-      <div className="h-full flex items-center">
-        <img src="/app-name.png" alt="Platform Name" className="h-full w-auto object-contain shrink-0 dark:hidden block" />
-        <img src="/app-name-dark.png" alt="Platform Name" className="h-full w-auto object-contain shrink-0 hidden dark:block" />
-      </div>
+      {/* Logo Mark (R Icon) */}
+      <img src="/app-logo.png" alt="Restops Logo" className="h-full w-auto object-contain shrink-0 dark:hidden block" />
+      <img src="/app-logo-dark.png" alt="Restops Logo" className="h-full w-auto object-contain shrink-0 hidden dark:block" />
+
+      {/* Brand Name (Native Text as per Guidelines) */}
+      {showText && (
+        <span className="font-bold tracking-tight text-xl text-black dark:text-white font-sans uppercase">
+          Restops
+        </span>
+      )}
     </div>
   );
 }
