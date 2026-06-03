984ec023556636efe54af2d0cd53e55098264fbd
Siva Sai Pavan Karthik Uppalapati <uppalapatisivasaipavankarthik@gmail.com>
2026-06-03 12:03:12 -0400

    03-06/04

--- code/src/components/RestopsLogo.jsx
+++ code/src/components/RestopsLogo.jsx
@@ -5,8 +5,8 @@ export default function RestopsLogo({ className = "h-12", showText = true }) {
     <div className={`flex items-center gap-3 ${className}`}>
       {/* Typography / Name Image */}
       <div className="h-full flex items-center">
-        <img src="/app-name.png" alt="Platform Name" className="h-[80%] w-auto object-contain shrink-0 transform scale-[1.5] origin-left dark:hidden block" />
-        <img src="/app-name-dark.png" alt="Platform Name" className="h-[80%] w-auto object-contain shrink-0 transform scale-[1.5] origin-left hidden dark:block" />
+        <img src="/app-name.png" alt="Platform Name" className="h-full w-auto object-contain shrink-0 dark:hidden block" />
+        <img src="/app-name-dark.png" alt="Platform Name" className="h-full w-auto object-contain shrink-0 hidden dark:block" />
       </div>
     </div>
   );
