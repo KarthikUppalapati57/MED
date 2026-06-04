81167a4d1d99ddab2bcdfabd585b4bc1feb33140
Siva Sai Pavan Karthik Uppalapati <uppalapatisivasaipavankarthik@gmail.com>
2026-06-04 13:15:57 -0400

    04-06/01

---
+++ code/android/app/src/main/java/com/restops/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.restops.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ code/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.restops.app',
+  appName: 'Restops',
+  webDir: 'dist'
+};
+
+export default config;
